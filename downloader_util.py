eb9365db770cf9eeff9ae7a9a3bd36d8a4275ce0
Pfreitas97 <pfreitas@purdue.edu>
2020-06-26 19:13:37 -0400

    Made downloader util more modular so it can be called for other programs and won't automatically download everything every time its run.

--- downloader_util.py
+++ downloader_util.py
@@ -24,34 +24,41 @@ AIRFOIL_COORD_URL = "https://m-selig.ae.illinois.edu/ads/archives/coord_seligFmt
 # NOTE: If you encounter issues with the propeller database, it is likely an issue with the server in that case
 # simply use the link below to download the files directly:
     
-PROP_BACKUP_URL = "https://m-selig.ae.illinois.edu/props/propDB.html"
 AIRFOIL_BACKUP_URL = "https://m-selig.ae.illinois.edu/ads/coord_database.html"
    
+def downloadPropellerData():
+    
+    
+    #Propeller Database url
+    PROPELLER_URL = "https://m-selig.ae.illinois.edu/props/download/UIUC-propDB.zip"
+    
+    #Webiste URL in case server is unavailable
+    PROP_BACKUP_URL = "https://m-selig.ae.illinois.edu/props/propDB.html"
+    r = requests.get(PROPELLER_URL)
+    if r.ok:
+        PROP_Zip = zipfile.ZipFile(io.BytesIO(r.content))
+        PROP_Zip.extractall()
+    else:
+        print("Oh oh server trouble... please download the file directly at %s it should be as \
+              simple as clicking a link." % PROP_BACKUP_URL)
+              
+    
+    
 
+def downloadAirfoilData():
+    rFoil = requests.get(AIRFOIL_COORD_URL)
+    if rFoil.ok:
+        AIR_Zip = zipfile.ZipFile(io.BytesIO(rFoil.content))
+        AIR_Zip.extractall()
+    else:
+        print("Oh oh server trouble... please download the file directly at %s it should be as simple as clicking a link. \
+              The Zip file you're looking for is in the Archives section. \
+                  Search for 'Zip archive' " % AIRFOIL_BACKUP_URL)
+    
+    
 
+    
 
-
-r = requests.get(PROPELLER_URL)
-
-if r.ok:
-    PROP_Zip = zipfile.ZipFile(io.BytesIO(r.content))
-    PROP_Zip.extractall()
-else:
-    print("Oh oh server trouble... please download the file directly at %s it should be as \
-          simple as clicking a link."
-            % PROP_BACKUP_URL)
-
-
-
-rFoil = requests.get(AIRFOIL_COORD_URL)
-
-if rFoil.ok:
-    AIR_Zip = zipfile.ZipFile(io.BytesIO(rFoil.content))
-    AIR_Zip.extractall()
-else:
-    print("Oh oh server trouble... please download the file directly at %s it should be as simple as clicking a link. \
-           The Zip file you're looking for is in the Archives section. Search for 'Zip archive' "
-           % AIRFOIL_BACKUP_URL)
 
 
 
